f9c4372a682ce120e258e5bc7d1b07b957f2494d
bothegerman <bjpet234@learner.dvusd.org>
2026-01-13 13:53:29 -0700

    tuesday update
    change no 2

--- pyfest-realone/pyfest-realone/pyfest_realone.py
+++ pyfest-realone/pyfest-realone/pyfest_realone.py
@@ -1,6 +1,21 @@
+
+lineup = [("The Pythonistas", "Rock", 45),
+    ("Code Play", "Indie", 30),
+    ("Syntax Error", "Metal", 60)]
+
 def print_lineup(bands):
-    total_time = sum
+    total_time = sum[time]
     print("Current Lineup: {lineup}")
+    print("Total Festival Duration: {total_time} minutes")
+
+def add_band(bands):
+    name = input("Enter band name: ")
+    genre = input("Enter band genre: ")
+    time = int(input("Enter performance time (in minutes): "))
+    lineup.append((name, genre, time))
+    print(f"Added {name} with a performance time of {time} minutes.")
+
+
 
 # start
 print("\n--- Py-Fest 2026 Stage Manager ---")
@@ -14,3 +29,13 @@ choice = int(input("Select an option (1-6): "))
 
 if choice == 1:
     print_lineup(bands)
+
+elif choice == 2:
+    add_band(bands)
+
+elif choice == 3:
+elif choice == 4:
+elif choice == 5:
+
+elif choice == 6:
+    print("Exiting Py-Fest 2026 Stage Manager. Goodbye!")
